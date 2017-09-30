f963508bb24cb4ea1788ef2ae5f62126e4af0891
toophy <toophy@vip.qq.com>
2017-09-30 08:53:06 +0800

    修改网络协议

--- examples/echo/server/msg_proc.go
+++ examples/echo/server/msg_proc.go
@@ -11,37 +11,23 @@ import (
 
 func Broadcast_Scene_Skill(a *AccountData, data *EchoProto) {
 
+	ret, _ := json.Marshal(struct {
+		C    string                 `json:"c"`
+		M    string                 `json:"m"`
+		Data map[string]interface{} `json:"data"`
+		Ret  string                 `json:"ret"`
+		Msg  string                 `json:"msg"`
+	}{
+		C:    data.C,
+		M:    data.M,
+		Data: data.Data,
+		Ret:  "ok",
+		Msg:  ""})
+
 	for k, _ := range gAccounts {
 		if gAccounts[k].Account == a.Account {
 			continue
 		}
-
-		// 技能目标
-		// 技能xx
-		retData := struct {
-			Name    string `json:"name"` // 技能name
-			Account string `json:"account"`
-			IsDown  bool   `json:"isDown"`
-			Ret     string `json:"ret"`
-		}{
-			data.Data["name"].(string),
-			a.Account,
-			data.Data["isDown"].(bool),
-			"ok"}
-
-		ret, _ := json.Marshal(struct {
-			C    string `json:"c"`
-			M    string `json:"m"`
-			Data struct {
-				Name    string `json:"name"` // 技能name
-				Account string `json:"account"`
-				IsDown  bool   `json:"isDown"`
-				Ret     string `json:"ret"`
-			} `json:"data"`
-		}{
-			C:    "Scene",
-			M:    "Skill",
-			Data: retData})
 
 		err := gAccounts[k].C.WriteMessage(a.Mt, ret)
 		if err != nil {
@@ -52,29 +38,20 @@ func Broadcast_Scene_Skill(a *AccountData, data *EchoProto) {
 
 func Scene_Skill(a *AccountData, mt int, data *EchoProto) bool {
 
-	// 技能目标
-	// 技能xx
-	retData := struct {
-		Name    string `json:"name"` // 技能name
-		Account string `json:"account"`
-		Ret     string `json:"ret"`
-	}{
-		data.Data["name"].(string),
-		a.Account,
-		"ok"}
+	data.Data["account"] = a.Account
 
 	ret, _ := json.Marshal(struct {
-		C    string `json:"c"`
-		M    string `json:"m"`
-		Data struct {
-			Name    string `json:"name"` // 技能name
-			Account string `json:"account"`
-			Ret     string `json:"ret"`
-		} `json:"data"`
+		C    string                 `json:"c"`
+		M    string                 `json:"m"`
+		Data map[string]interface{} `json:"data"`
+		Ret  string                 `json:"ret"`
+		Msg  string                 `json:"msg"`
 	}{
 		C:    data.C,
 		M:    data.M,
-		Data: retData})
+		Data: data.Data,
+		Ret:  "ok",
+		Msg:  ""})
 
 	err := a.C.WriteMessage(mt, ret)
 	if err != nil {
@@ -89,47 +66,23 @@ func Scene_Skill(a *AccountData, mt int, data *EchoProto) bool {
 
 func Broadcast_Scene_PlayerEnter(a *AccountData, data *EchoProto) {
 
-	data.C = "Scene"
-	data.M = "PlayerEnter"
+	ret, _ := json.Marshal(struct {
+		C    string                 `json:"c"`
+		M    string                 `json:"m"`
+		Data map[string]interface{} `json:"data"`
+		Ret  string                 `json:"ret"`
+		Msg  string                 `json:"msg"`
+	}{
+		C:    "Scene",
+		M:    "PlayerEnter",
+		Data: data.Data,
+		Ret:  "ok",
+		Msg:  ""})
 
 	for k, _ := range gAccounts {
 		if gAccounts[k].Account == a.Account {
 			continue
 		}
-
-		pos_x, _ := data.Data["pos_x"].(int)
-		pos_y, _ := data.Data["pos_y"].(int)
-
-		retData := struct {
-			Account string `json:"account"`
-			Pwd     string `json:"pwd"`
-			PosX    int    `json:"pos_x"`
-			PosY    int    `json:"pos_y"`
-			Ret     string `json:"ret"`
-			Msg     string `json:"msg"`
-		}{
-			data.Data["account"].(string),
-			data.Data["pwd"].(string),
-			pos_x,
-			pos_y,
-			"ok",
-			""}
-
-		ret, _ := json.Marshal(struct {
-			C    string `json:"c"`
-			M    string `json:"m"`
-			Data struct {
-				Account string `json:"account"`
-				Pwd     string `json:"pwd"`
-				PosX    int    `json:"pos_x"`
-				PosY    int    `json:"pos_y"`
-				Ret     string `json:"ret"`
-				Msg     string `json:"msg"`
-			} `json:"data"`
-		}{
-			C:    data.C,
-			M:    data.M,
-			Data: retData})
 
 		err := gAccounts[k].C.WriteMessage(gAccounts[k].Mt, ret)
 		if err != nil {
@@ -140,32 +93,25 @@ func Broadcast_Scene_PlayerEnter(a *AccountData, data *EchoProto) {
 
 func Broadcast_Scene_PlayerLeave(a *AccountData) {
 
+	data := map[string]interface{}{"account": a.Account}
+
+	ret, _ := json.Marshal(struct {
+		C    string                 `json:"c"`
+		M    string                 `json:"m"`
+		Data map[string]interface{} `json:"data"`
+		Ret  string                 `json:"ret"`
+		Msg  string                 `json:"msg"`
+	}{
+		C:    "Scene",
+		M:    "PlayerLeave",
+		Data: data,
+		Ret:  "ok",
+		Msg:  ""})
+
 	for k, _ := range gAccounts {
 		if gAccounts[k].Account == a.Account {
 			continue
 		}
-
-		retData := struct {
-			Account string `json:"account"`
-			Ret     string `json:"ret"`
-			Msg     string `json:"msg"`
-		}{
-			a.Account,
-			"ok",
-			""}
-
-		ret, _ := json.Marshal(struct {
-			C    string `json:"c"`
-			M    string `json:"m"`
-			Data struct {
-				Account string `json:"account"`
-				Ret     string `json:"ret"`
-				Msg     string `json:"msg"`
-			} `json:"data"`
-		}{
-			C:    "Scene",
-			M:    "PlayerLeave",
-			Data: retData})
 
 		err := gAccounts[k].C.WriteMessage(gAccounts[k].Mt, ret)
 		if err != nil {
