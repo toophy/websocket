1985ed8d0104fc47a270b3c8e3f394b5130d1cae
toophy <toophy@vip.qq.com>
2017-11-09 14:15:47 +0800

    基本完成tilemap碰撞

--- examples/echo/server/util/tile_map.go
+++ examples/echo/server/util/tile_map.go
@@ -1,5 +1,13 @@
 package util
 
+const (
+	DirLeft  = 1 // 向左
+	DirRight = 2 // 向右
+	DirUp    = 3 // 向上
+	DirDown  = 4 // 向下
+)
+
+// Rect 矩形
 type Rect struct {
 	X int32
 	Y int32
@@ -7,51 +15,37 @@ type Rect struct {
 	H int32
 }
 
-// 碰撞
+// CrossEx 碰撞
 func (r *Rect) CrossEx(a Rect) bool {
 	return r.Cross(a.X, a.Y, a.W, a.H)
 }
 
-// 碰撞
+// Cross 碰撞
 func (r *Rect) Cross(x, y, w, h int32) bool {
-
-	aMaxX := x + w
-	aMaxY := y + h
-	bMaxX := r.X + r.W
-	bMaxY := r.Y + r.H
-
-	// a is left of b
-	if aMaxX < r.X {
+	if x+w-1 < r.X {
 		return false
 	}
-
-	// a is right of b
-	if x > bMaxX {
+	if x > r.X+r.W-1 {
 		return false
 	}
-
-	// a is above b
-	if aMaxY < r.Y {
+	if y+h-1 < r.Y {
 		return false
 	}
-
-	// a is below b
-	if y > bMaxY {
+	if y > r.Y+r.H-1 {
 		return false
 	}
-
-	// The two overlap
 	return true
 }
 
+// Obj 地图对象
 type Obj struct {
-	Id    int16
+	Id    int32
 	Pos   Rect
 	Cells []int32 // 最后一次染色单元格
 }
 
 type Cell struct {
-	Objs map[int16]*Obj
+	Objs map[int32]*Obj
 }
 
 // 染色 tile, 一个物品可以挂在多个tile下面, 用来进行碰撞抽检, 用ID作为碰撞顺序标准
@@ -67,6 +61,7 @@ type TileMap struct {
 	Cells     []Cell
 }
 
+// Init 初始化TileMap
 func (q *TileMap) Init(tileW, tileH, realW, realH int32) bool {
 
 	if tileW < 1 || tileH < 1 || realW < 0 || realH < 0 || tileW > realW || tileH > realH {
@@ -91,45 +86,39 @@ func (q *TileMap) Init(tileW, tileH, realW, realH int32) bool {
 	return true
 }
 
-// 由具体的坐标获取cell坐标
-func (q *TileMap) GetCellId(x, y int32) (int32, int32, bool) {
-	if x < 0 || y < 0 || x > q.realW || y > q.realH {
-		return 0, 0, false
-	}
-	return x / q.tileW, y / q.tileH, true
-}
-
-func (q *TileMap) GetPointCell(x, y int32) int32 {
-	if x < 0 || y < 0 || x > q.realW || y > q.realH {
-		return -1
-	}
-	return y/q.tileH*q.W + x/q.tileW
-}
-
-func (q *TileMap) GetRectCell(x, y, w, h int32) []int32 {
-	if x < 0 || y < 0 || w < 0 || h < 0 || x+w >= q.realW || y+h >= q.realH {
+// GetCrossCells 获取矩形交叉的Cell
+func (q *TileMap) GetCrossCells(x, y, w, h int32) (rets []int32) {
+	if x < 0 || y < 0 || w < 0 || h < 0 || x+w > q.realW || y+h > q.realH {
 		return []int32{}
 	}
-	rets := []int32{}
 	x2 := x + w
 	y2 := y + h
 	cx := x / q.tileW
 	cy := y / q.tileH
 	cx2 := x2 / q.tileW
 	cy2 := y2 / q.tileH
-	for r := cy; r < cy2; r++ {
+	for r := cy; r <= cy2; r++ {
 		for c := cx; c <= cx2; c++ {
 			rets = append(rets, r*q.W+c)
 		}
 	}
-	return rets
+	return
 }
 
-func (q *TileMap) CanInsert(x, y, w, h int32) bool {
-	newCells := q.GetRectCell(x, y, w, h)
+// CanInsert 检查对象使用新矩形能否插入
+func (q *TileMap) CanInsert(x, y, w, h int32, o *Obj) bool {
+	if o == nil {
+		return false
+	}
+
+	newCells := q.GetCrossCells(x, y, w, h)
+	if len(newCells) == 0 {
+		return false
+	}
+
 	for _, v := range newCells {
-		for _, cv := range q.Cells[v-1].Objs {
-			if cv.Pos.Cross(x, y, w, h) {
+		for _, cv := range q.Cells[v].Objs {
+			if o != cv && cv.Pos.Cross(x, y, w, h) {
 				return false
 			}
 		}
@@ -138,49 +127,50 @@ func (q *TileMap) CanInsert(x, y, w, h int32) bool {
 	return true
 }
 
-func (q *TileMap) Insert(o *Obj) bool {
-	if o == nil {
-		return false
+// Insert 进行插入
+func (q *TileMap) Insert(o *Obj, newRect *Rect) bool {
+	if o != nil {
+		if newRect != nil {
+			o.Pos = *newRect
+		}
+		if q.CanInsert(o.Pos.X, o.Pos.Y, o.Pos.W, o.Pos.H, o) {
+			// 褪色
+			for _, v := range o.Cells {
+				delete(q.Cells[v].Objs, o.Id)
+			}
+			// 染色
+			newCells := q.GetCrossCells(o.Pos.X, o.Pos.Y, o.Pos.W, o.Pos.H)
+			for _, v := range newCells {
+				q.Cells[v].Objs[o.Id] = o
+			}
+			o.Cells = newCells
+			return true
+		}
 	}
-
-	if !q.CanInsert(o.Pos.X, o.Pos.Y, o.Pos.W, o.Pos.H) {
-		return false
-	}
-
-	// 褪色
-	for _, v := range o.Cells {
-		delete(q.Cells[v-1].Objs, o.Id)
-	}
-
-	// 染色
-	newCells := q.GetRectCell(o.Pos.X, o.Pos.Y, o.Pos.W, o.Pos.H)
-	for _, v := range newCells {
-		q.Cells[v-1].Objs[o.Id] = o
-	}
-
-	o.Cells = newCells
-
-	return true
+	return false
 }
 
+// Erase 擦除一个对象
 func (q *TileMap) Erase(o *Obj) {
 	// 褪色
 	if o != nil {
 		for _, v := range o.Cells {
-			delete(q.Cells[v-1].Objs, o.Id)
+			delete(q.Cells[v].Objs, o.Id)
 		}
 	}
 	o.Cells = []int32{}
 }
 
+// Clear 清除所有对象痕迹
 func (q *TileMap) Clear() {
 	if q.initOk {
 		for i := uint32(0); i < q.CellCount; i++ {
-			q.Cells[i].Objs = make(map[int16]*Obj, 0)
+			q.Cells[i].Objs = make(map[int32]*Obj, 0)
 		}
 	}
 }
 
+// Destroy 摧毁TileMap
 func (q *TileMap) Destroy() {
 	if q.initOk {
 		q.W = 0
@@ -193,4 +183,27 @@ func (q *TileMap) Destroy() {
 		q.CellCount = 0
 		q.initOk = false
 	}
+}
+
+// ObjMove 对象在TileMap上移动,step是步长
+func (q *TileMap) ObjMove(o *Obj, dir int, step int32) bool {
+	if o == nil {
+		return false
+	}
+	newRect := o.Pos
+	switch dir {
+	case DirLeft:
+		newRect.X -= step
+		break
+	case DirRight:
+		newRect.X += step
+		break
+	case DirUp:
+		newRect.Y -= step
+		break
+	case DirDown:
+		newRect.Y += step
+		break
+	}
+	return q.Insert(o, &newRect)
 }
--- examples/echo/server/util/tile_map_test.go
+++ examples/echo/server/util/tile_map_test.go
@@ -15,18 +15,47 @@ func Test_Tilemap(t *testing.T) {
 	mapObjs := &MapObj{}
 	mapObjs.Init()
 
-	mapObjs.Objs[1] = &Obj{Id: 1, Pos: Rect{X: 1, Y: 1, W: 20, H: 20}, Cells: []int32{}}
-	mapObjs.Objs[2] = &Obj{Id: 2, Pos: Rect{X: 30, Y: 30, W: 20, H: 20}, Cells: []int32{}}
+	mapObjs.Objs[1] = &Obj{Id: 1, Pos: Rect{X: 0, Y: 0, W: 30, H: 20}, Cells: []int32{}}
+	mapObjs.Objs[2] = &Obj{Id: 2, Pos: Rect{X: 260, Y: 40, W: 20, H: 20}, Cells: []int32{}}
+	mapObjs.Objs[3] = &Obj{Id: 3, Pos: Rect{X: 300, Y: 40, W: 180, H: 20}, Cells: []int32{}}
 
 	mapA := &TileMap{}
-	mapA.Init(100, 100, 300, 300)
+	mapA.Init(100, 100, 500, 500)
 
-	if !mapA.Insert(mapObjs.Objs[1]) {
+	if !mapA.Insert(mapObjs.Objs[1], nil) {
 		t.Error("insert 1 failed")
 	}
 
-	if !mapA.Insert(mapObjs.Objs[2]) {
+	if !mapA.Insert(mapObjs.Objs[2], nil) {
 		t.Error("insert 2 failed")
 	}
 
+	if !mapA.Insert(mapObjs.Objs[3], nil) {
+		t.Error("insert 3 failed")
+	}
+
+	mapA.ObjMove(mapObjs.Objs[1], DirDown, 10)
+	mapA.ObjMove(mapObjs.Objs[1], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[1], DirDown, 10)
+	mapA.ObjMove(mapObjs.Objs[1], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[1], DirDown, 10)
+	mapA.ObjMove(mapObjs.Objs[1], DirDown, 10)
+	mapA.ObjMove(mapObjs.Objs[1], DirDown, 10)
+
+	t.Error("rect ", mapObjs.Objs[1].Pos)
+
+	mapA.ObjMove(mapObjs.Objs[3], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[3], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[3], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[3], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[3], DirRight, 10)
+
+	t.Error("rect3 ", mapObjs.Objs[3].Pos)
+
+	mapA.ObjMove(mapObjs.Objs[2], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[2], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[2], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[2], DirRight, 10)
+	mapA.ObjMove(mapObjs.Objs[2], DirRight, 10)
+	t.Error("rect2 ", mapObjs.Objs[2].Pos)
 }
