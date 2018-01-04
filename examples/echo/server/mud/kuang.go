2721bdaeded29b7a54967083402aaf371c15bdca
toophy <toophy@vip.qq.com>
2018-01-04 17:47:03 +0800

    功能图

--- examples/echo/server/mud/kuang.go
+++ examples/echo/server/mud/kuang.go
@@ -8,9 +8,9 @@ import (
 )
 
 type AuctionLog struct {
-	AccName string // 竞拍人帐号
-	Count   int64  // 货币数量
-	Time    int64  // 竞拍时间
+	AccName   string // 竞拍人帐号
+	ItemCount int64  // 货币数量
+	Time      int64  // 竞拍时间
 }
 
 // AuctionSession 拍卖, 拍卖时冻结指定的 Jewel 数量
@@ -72,6 +72,7 @@ func GetKuangSys() *KuangSys {
 // 每秒一次计算, 每分钟一次计算
 func (k *KuangSys) Update() {
 	t := time.NewTimer(1 * time.Second)
+	t2 := time.NewTimer(2 * time.Second)
 
 	for {
 		select {
@@ -91,6 +92,53 @@ func (k *KuangSys) Update() {
 			}
 
 			t.Reset(1 * time.Second)
+			break
+
+		case <-t2.C:
+			for i, v := range k.JewelAuctions {
+				if len(v.Sessions) > 0 {
+					if _, ok := v.Sessions[v.LastSessionID]; ok {
+						if !v.Sessions[v.LastSessionID].AuctionOver {
+							if time.Now().Unix() > v.Sessions[v.LastSessionID].EndTime {
+								v.Sessions[v.LastSessionID].AuctionOver = true
+								// 拍卖场次结束, 钻石归属, 玩家通用货币归属
+								// 直接把钻石邮件给玩家, 同时扣除玩家的通货(ItemID,ItemCount)
+								// 系统级别的脚本邮件, 也就是唯一的脚本邮件形式
+								// 邮件系统接收到该邮件, 如果玩家在线, 自动执行, 不在线, 等上线时执行
+								// 此类邮件永久保存
+								//
+								// 邮件类型 : 脚本邮件
+								// 邮件标题 : 拍卖场竞价成功
+								// 文本内容 : 恭喜你在xxx拍卖场xxx次拍卖会,使用xx通货成功竞价获取xx钻石
+								//            收到邮件后, 扣除xx通货xx个, 获取钻石xx个
+								// 脚本内容 : AutionSuccess(xx钻石数量,xx通货ID,xx通货数量)
+
+								// 	type Mail struct {
+								// 	ID            int64  // ID
+								// 	Title         string // 标题
+								// 	SenderType    string // 发送人类型: 玩家, 系统, 战队
+								// 	SenderName    string // 发送人名称: SYSTEM,TEAM,Account
+								// 	RecverType    string // 接收人类型: 玩家, 系统, 战队
+								// 	RecverName    string // 接收人名称: SYSTEM,TEAM,Account
+								// 	Content       string // 文本内容(还有客户端脚本)
+								// 	Script        string // 脚本内容: 供服务端解析执行
+								// 	Read          bool   // 是否已读
+								// 	RecvAccessory bool   // 是否接收了附件
+								// 	SendTime      int64  // 发送时间
+								// 	DestoryTime   int64  // 保存截止时间
+								// }
+								newMail := &Mail{
+									ID : 0,
+									Title:"拍卖场竞价成功",
+									SenderType:""
+								}
+							}
+						}
+					}
+				}
+			}
+
+			t2.Reset(2 * time.Second)
 			break
 		}
 	}
@@ -132,11 +180,11 @@ func (a *KuangSys) CreateAuctionSession(itemID int64, jewelCount int64, accName 
 		}
 
 		if len(j.Sessions) > 0 {
-			if _, ok := j.Sessions[j.LastID]; ok {
-				if !j.Sessions[j.LastID].AuctionOver {
+			if _, ok := j.Sessions[j.LastSessionID]; ok {
+				if !j.Sessions[j.LastSessionID].AuctionOver {
 					return errors.New("拍卖会还在进行中")
 				}
-				lastItemCounts = j.Sessions[j.LastID].LastCounts
+				lastItemCounts = j.Sessions[j.LastSessionID].LastCounts
 			}
 		}
 
@@ -159,7 +207,7 @@ func (a *KuangSys) CreateAuctionSession(itemID int64, jewelCount int64, accName 
 		as.AuctionOver = false
 		as.Logs = make([]*AuctionLog, 0)
 
-		al := AuctionLog{AccName: accName, Count: itemCount, Time: time.Now().Unix()}
+		al := AuctionLog{AccName: accName, ItemCount: itemCount, Time: time.Now().Unix()}
 
 		j.Sessions[as.ID] = as
 
@@ -170,39 +218,49 @@ func (a *KuangSys) CreateAuctionSession(itemID int64, jewelCount int64, accName 
 	return errors.New("没有这种通货拍卖场")
 }
 
-func (a *KuangSys) AskAuction(name string, accName string, itemCount int64) bool {
+// AskAuction 请求拍卖会竞价
+func (a *KuangSys) AskAuction(itemID int64, accName string, itemCount int64) error {
 
-	if j, ok := a.JewelAuctions[name]; ok {
+	if j, ok := a.JewelAuctions[itemID]; ok {
 
-		lastCounts := 0 // 货币数量
+		lastItemCounts := 0 // 货币数量
 
 		if len(accName) < 1 {
-			return false
+			return errors.New("帐号不存在")
 		}
-		// 对accName检查
 
 		if itemCount < 1 {
-			return false
+			return errors.New("通货数量要>0")
 		}
 
-		// 1. 当前拍卖是否结束
 		if len(j.Sessions) > 0 {
-			if _, ok := j.Sessions[j.LastID]; ok {
-				if !j.Sessions[j.LastID].AuctionOver {
-					return false
+			if _, ok := j.Sessions[j.LastSessionID]; ok {
+				if j.Sessions[j.LastSessionID].AuctionOver {
+					return errors.New("拍卖会已经结束")
 				}
-				lastCounts = j.Sessions[j.LastID].LastCounts
+				lastItemCounts = j.Sessions[j.LastSessionID].LastCounts
+
+				if len(j.Sessions[j.LastSessionID].Logs) > 0 {
+					if j.Sessions[j.LastSessionID].Logs[len(j.Sessions[j.LastSessionID].Logs)-1].AccName == accName {
+						return errors.New("同一个账号不可以连续竞价")
+					}
+				}
 			}
 		}
 
-		// 2. jewelCount 是否正确, 到矿石系统检查
-		if !GetKuangSys().AskFreezeJewel(j.KuangName, jewelCount) {
-			return false
+		// 每次至少增加 1%
+		if itemCount < int64(lastItemCounts*1.1) {
+			return errors.New("至少增加1%进行竞价")
 		}
 
-		// 3. itemCount 是否合法, 基础竞价
-		if itemCount < int64(lastCounts*0.9) {
-			return false
-		}
+		nA := &AuctionLog{AccName: accName, ItemCount: itemCount, Time: time.Now().Unix()}
+
+		j.Sessions[j.LastSessionID].LastCounts = itemCount
+		j.Sessions[j.LastSessionID].Logs = append(j.Sessions[j.LastSessionID].Logs, nA)
+
+		// 玩家无法赖账, 因为通货有多个入口(+), 但是只有拍卖场一个出口(而且只有一个拍卖机会-当前场次), 过了当前场次, 竞价作废, 重新再来
+		return nil
 	}
+
+	return errors.New("没有这种通货拍卖场")
 }
--- examples/echo/server/mud/mail.go
+++ examples/echo/server/mud/mail.go
@@ -1,3 +1,101 @@
 package mud
 
 // 邮件
+// 邮件类型 : 脚本邮件
+// 邮件标题 : 拍卖场竞价成功
+// 文本内容 : 恭喜你在xxx拍卖场xxx次拍卖会,使用xx通货成功竞价获取xx钻石
+//            收到邮件后, 扣除xx通货xx个, 获取钻石xx个
+// 脚本内容 : AutionSuccess(xx钻石数量,xx通货ID,xx通货数量)
+
+// Hall 大厅
+type Mail struct {
+	ID            int64  // ID
+	Title         string // 标题
+	SenderType    string // 发送人类型: 玩家, 系统, 战队
+	SenderName    string // 发送人名称: SYSTEM,TEAM,Account
+	RecverType    string // 接收人类型: 玩家, 系统, 战队
+	RecverName    string // 接收人名称: SYSTEM,TEAM,Account
+	Content       string // 文本内容(还有客户端脚本)
+	Script        string // 脚本内容: 供服务端解析执行
+	Read          bool   // 是否已读
+	RecvAccessory bool   // 是否接收了附件
+	SendTime      int64  // 发送时间
+	DestoryTime   int64  // 保存截止时间
+}
+
+type MailSys struct {
+	Mails  map[int64]*Mail
+	LastID int64
+}
+
+var (
+	mailSys *MailSys
+)
+
+func init() {
+	mailSys = &MailSys{}
+	mailSys.Mails = make(map[int64]*Mail, 0)
+	MailSys.LastID = 1
+}
+
+// GetMailSys 获取邮件系统
+func GetMailSys() *MailSys {
+	return mailSys
+}
+
+// Send 发送邮件
+func (m *MailSys) Send(mail *Mail) {
+	mail.ID = mailSys.LastID
+	mailSys.LastID++
+	m.Mails[mail.ID] = mail
+}
+
+// GetMails 索取所有邮件(可以考虑分段索取)
+func (m *MailSys) GetMails(recverType string, recverName string) (ret []Mail) {
+	for k, v := range m.Mails {
+		if v.RecverType == recverType && v.RecverName == recverName {
+			ret = append(ret, *v)
+		}
+	}
+	return
+}
+
+// SetMailRead 设置邮件已经读取的标记
+func (m *MailSys) SetMailRead(mailID int64) {
+	if _, ok := m.Mails[mailID]; ok {
+		m.Mails[mailID].Read = true
+	}
+}
+
+// SetMailRecvAccessory 设置附件已经索取的标记
+func (m *MailSys) SetMailRecvAccessory(mailID int64) {
+	if _, ok := m.Mails[mailID]; ok {
+		m.Mails[mailID].RecvAccessory = true
+	}
+}
+
+// DestoryMail 摧毁指定邮件(附件已经索取)
+func (m *MailSys) DestoryMail(mailID int64) {
+	if _, ok := m.Mails[mailID]; ok {
+		if m.Mails[mailID].RecvAccessory {
+			delete(m.Mails, mailID)
+		}
+	}
+}
+
+// DestoryMailByName 通过接收者名称,类型消耗附件已经索取的邮件
+func (m *MailSys) DestoryMailByName(recverType string, recverName string) {
+	ret := []int64{}
+
+	for k, v := range m.Mails {
+		if v.RecverType == recverType && v.RecverName == recverName {
+			if v.RecvAccessory {
+				ret = append(ret, v.ID)
+			}
+		}
+	}
+
+	for i := 0; i < len(ret); i++ {
+		delete(m.Mails, ret[i])
+	}
+}
